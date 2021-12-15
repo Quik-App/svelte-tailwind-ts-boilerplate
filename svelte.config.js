f5ccd09ad53ab7ff24dbf01a2646b724e1b6b184
Achudh Krishna A G <achudhk@gmail.com>
2021-12-16 01:43:03 +0530

    init commit

--- svelte.config.js
+++ svelte.config.js
@@ -2,20 +2,17 @@ import preprocess from 'svelte-preprocess';
 import adapterStatic from '@sveltejs/adapter-static';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import path from 'path';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: {
-				plugins: [
-				 tailwindcss,
-				 autoprefixer
-				],
+				plugins: [tailwindcss, autoprefixer]
 			}
-		}),
+		})
 	],
 
 	kit: {
@@ -23,6 +20,14 @@ const config = {
 		target: '#svelte',
 		adapter: {
 			adapt: adapterStatic
+		},
+		vite: {
+			resolve: {
+				alias: {
+					$assets: path.resolve('./src/static'),
+					$base: path.resolve('./')
+				}
+			}
 		}
 	}
 };
